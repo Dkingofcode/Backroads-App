03fc044e3003b8c7a9b7082181cd3aedd21c16de
Dkingofcode <doladepo128@gmail.com>
2023-03-26 22:13:00 +0100

    Converting html and css to single react components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+//import books from './data';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Services from './components/services';
+import Tours from './components/Tours';
+import Footer from './components/Footer'; 
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar />
+    <Hero />
+    <About />
+    <Services />
+    <Tours />
+    <Footer />    
+    </>
+ );
 }
+  
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,51 @@
+// import img1 from './img/images1.jpg';
+// import img2 from './img/images2.jpeg';
+
+
+// const books = [
+//     {
+//         id: 1,
+//         img: img1,
+//         title: "Atomic Habits",
+//         author: "James Clear",
+//     },
+//     {
+//         id: 2,
+//         img: img2,
+//         title: "Atomic Habits",
+//         author: "James Clear",
+//     },
+//     {
+//         id: 3,
+//         img: img1,
+//         title: "Atomic Habits",
+//         author: "James Clear",
+//     },
+//     {
+//         id: 4,
+//         img: img2,
+//         title: "Atomic Habits",
+//         author: "James Clear",
+//     }
+// ]
+
+// export default books;
+
+export const pageLinks = [{ id: 1, href: '#home', text: 'home'},
+{ id: 2, href: '#about', text: 'about'},
+{ id: 3, href: '#services', text: 'services'},
+{ id: 4, href: '#tours', text: 'tours'},
+]
+
+export const socialLinks = [ 
+  { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
+  { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
+  { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+];
+
+
+export const services = [
+    {id: 1, icon: 'fas fa-wallet fa-fw', title: 'saving money', text: 'Lorem ipsum dolor sit amet consectetur aduisciping elit. Asperios, officia'},
+    {id: 2, icon: 'fas fa-tree fa-fw', title: 'endless hiking', text: 'Lorem ipsum dolor sit amet consectetur aduisciping elit. Asperios, officia'},
+    {id: 3, icon: 'fas fa-socks fa-fw', title: 'saving money', text: 'Lorem ipsum dolor sit amet consectetur aduisciping elit. Asperios, officia'},
+]
